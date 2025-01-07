b6b35dca1232cfb5a14ee2965b5b7903d4d73d35
Mohammed Faisal Khan <76160659+mdfaiskhan@users.noreply.github.com>
2025-01-07 15:59:10 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import aftabpic from './aftabpic.jpg'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://big-butcher-backend.onrender.com'
 export const currency = '₹'
 
 export const assets ={
