8a9c4e7958b6b0c794efb09c8315e0265ec2c4e7
Mohammed Faisal Khan <76160659+mdfaiskhan@users.noreply.github.com>
2025-01-09 00:29:04 +0530

    Update FreshMeatPage.jsx

--- frontend/src/components/FreshMeatPage/FreshMeatPage.jsx
+++ frontend/src/components/FreshMeatPage/FreshMeatPage.jsx
@@ -13,10 +13,10 @@ const FreshMeatPage = () => {
         <h2>Buy Beef Online in Bangalore</h2>
         <h3>Delivered Fresh, Halal & Convenient</h3>
         <p>
-          Looking for fresh, halal meat in Bangalore? Protein Fresh offers premium beef, chicken, lamb, and more, delivered directly to your door. Enjoy top-quality, fresh meat with ease and convenience – all with our fast, reliable delivery service.
+          Looking for fresh, halal meat in Bangalore? Big Butcher offers premium beef, chicken, lamb, and more, delivered directly to your door. Enjoy top-quality, fresh meat with ease and convenience – all with our fast, reliable delivery service.
         </p>
         <p>
-          Make your meals easier and tastier by choosing Protein Fresh – the best way to buy fresh, halal meat online in Bangalore.
+          Make your meals easier and tastier by choosing Big Butcher – the best way to buy fresh, halal meat online in Bangalore.
         </p>
         <button className="shop-now-btn">Shop Now</button>
       </div>
