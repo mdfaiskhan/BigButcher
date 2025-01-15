8ff5dce65fb933d5111a7990d14b5df8b84268ec
Mohammed Faisal Khan <76160659+mdfaiskhan@users.noreply.github.com>
2025-01-15 19:10:03 +0530

    Update BestSellers.jsx

--- frontend/src/components/BestSellers/BestSellers.jsx
+++ frontend/src/components/BestSellers/BestSellers.jsx
@@ -64,7 +64,7 @@ const BestSellers = () => {
   return (
     <div className="food-category">
       <h2 className="text-4xl font-bold mb-6 text-center text-gray-800">Best Sellers</h2>
-      <h3>Top Trending Products in Bengaluru</h3>
+      <h3>Best-Selling Meats in Bengaluru 🥩🍗</h3>
       <div className="food-category-slider-wrapper" {...swipeHandlers}>
         {/* Left Button (visible only on laptop and above) */}
         {!isMobile && (
